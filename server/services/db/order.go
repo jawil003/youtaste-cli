bfdacd41c0286d3ceec318089e4379817e449105
Jannik Will <jw19980309@gmail.com>
2022-02-04 23:16:21 +0100

    Update Server

--- server/services/db/order.go
+++ server/services/db/order.go
@@ -3,10 +3,8 @@ package db
 import (
 	"bs-to-scrapper/server/models"
 	"encoding/json"
-	"errors"
 	_ "github.com/thoas/go-funk"
 	bolt "go.etcd.io/bbolt"
-	"strings"
 )
 
 func (_ ServiceCollection) Order() OrderService {
@@ -32,34 +30,15 @@ func (o OrderService) GetByUser(user string) (*[]models.Order, error) {
 
 		value := b.Get([]byte(user))
 
-		valueString := string(value)
-
-		var arrayRes []string
-
-		err = json.Unmarshal([]byte(valueString), &arrayRes)
-
-		if err != nil {
-			return err
-		}
-
-		if len(arrayRes) == 0 {
+		if value == nil {
 			orderArray = []models.Order{}
 			return nil
 		}
 
-		for _, orderJson := range arrayRes {
+		err = json.Unmarshal(value, &orderArray)
 
-			if orderJson == "" {
-				continue
-			}
-
-			order, err := models.ToOrder(orderJson)
-
-			if err != nil {
-				return err
-			}
-
-			orderArray = append(orderArray, order)
+		if err != nil {
+			return err
 		}
 
 		return nil
@@ -89,36 +68,25 @@ func (o OrderService) Create(orders []models.Order, user string) error {
 
 		value := b.Get([]byte(user))
 
-		var jsonOrders []string
+		var arrayRes []models.Order
 
-		for _, order := range orders {
-			jsonOrder, err := order.ToJSON()
-
-			if err != nil {
-				return err
-			}
-
-			jsonOrders = append(jsonOrders, string(jsonOrder))
-		}
-
-		if len(value) > 0 {
-			value := b.Get([]byte(user))
-
-			var arrayRes []string
-
+		if value != nil {
 			err := json.Unmarshal(value, &arrayRes)
 
 			if err != nil {
 				return err
 			}
+		}
 
-			jsonArray, err := json.Marshal(append(arrayRes, jsonOrders...))
+		if len(arrayRes) > 0 {
+
+			jsonArray, err := json.Marshal(append(arrayRes, orders...))
 
 			if err != nil {
 				return err
 			}
 
-			err = b.Put([]byte(user), []byte(jsonArray))
+			err = b.Put([]byte(user), jsonArray)
 
 			if err != nil {
 				return err
@@ -127,90 +95,13 @@ func (o OrderService) Create(orders []models.Order, user string) error {
 			return nil
 		} else {
 
-			jsonArray, err := json.Marshal(append(jsonOrders, jsonOrders...))
+			jsonArray, err := json.Marshal(orders)
 
 			if err != nil {
 				return err
 			}
 
-			err = b.Put([]byte(user), []byte(jsonArray))
-			if err != nil {
-				return err
-			}
-		}
-
-		return nil
-	})
-
-	CloseConnection(db)
-
-	if err != nil {
-		return err
-	}
-
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (o OrderService) Update(orders []models.Order, user string) error {
-	db, err := OpenDbConnection()
-
-	if err != nil {
-		CloseConnection(db)
-		return err
-	}
-
-	err = db.Update(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte("orders"))
-
-		value := string(b.Get([]byte(user)))
-
-		if value == "" {
-			return errors.New("no user found")
-		}
-
-		var jsonOrders []string
-
-		for _, order := range orders {
-			jsonOrder, err := order.ToJSON()
-
-			if err != nil {
-				return err
-			}
-
-			jsonOrders = append(jsonOrders, string(jsonOrder))
-		}
-
-		if len(value) > 0 {
-			value := b.Get([]byte(user))
-
-			arrayRes := strings.Split(string(value), ",")
-
-			jsonArray, err := json.Marshal(append(arrayRes, jsonOrders...))
-
-			if err != nil {
-				return err
-			}
-
-			err = b.Put([]byte(user), []byte(jsonArray))
-
-			if err != nil {
-				return err
-			}
-
-			return nil
-		} else {
-
-			jsonArray, err := json.Marshal(append(jsonOrders, jsonOrders...))
-
-			if err != nil {
-				return err
-			}
-
-			err = b.Put([]byte(user), []byte(jsonArray))
+			err = b.Put([]byte(user), jsonArray)
 			if err != nil {
 				return err
 			}
